2f6d04447a6532db7ac9cfbcc4d32ca138e28591
Jeffrey Shurak <jshurak@anexinet.com>
2018-12-01 22:16:17 -0500

    success!

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-setup.sh & /opt/mssql/bin/sqlservr # & sleep 40; for filename in /dba/*.sql; do /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P@$$w0rd' -i "$filename"; done
+/dba/setup.sh & /opt/mssql/bin/sqlservr  # & sleep 40; for filename in /dba/*.sql; do /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P@$$w0rd' -i "$filename"; done
