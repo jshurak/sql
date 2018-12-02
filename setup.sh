24036ad98574b33f7d9194a735db460927366ef1
Jeffrey Shurak <jshurak@anexinet.com>
2018-12-01 20:59:40 -0500

    failed logins

--- setup.sh
+++ setup.sh
@@ -1,3 +1,1 @@
-sleep 40
-
-for filename in /dba/*.sql; do /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P@$$w0rd' -i "$filename"; done
+sleep 40s; for filename in /dba/*.sql; do /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'P@ssW0rd' -i "$filename"; done
